63f2c12500d03ca495bb4e985f5797e3726ce951
Its-a-me-Ashwin <56929681+Its-a-me-Ashwin@users.noreply.github.com>
2020-02-11 17:24:37 +0530

    Add files via upload

---
+++ input.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+// This is simple code in GOlang
+
+/* Here we do sufff i like bois */
+func main() {
+	int a := 0
+	"HELLO BOIS"
+	10.00
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+	1000.0
+	10.55
+	0.555
+	69
+	55
+	"YO MAMA"
+}
+
+
+<END>
